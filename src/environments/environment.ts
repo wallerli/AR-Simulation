f33c42b026d5af9d57423bf0672967054ef2eebe
michael-small <small203@morris.umn.edu>
2019-06-25 10:02:54 -0500

    Read firebase JSON secret file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+const urls = require("../assets/firebase.json"); //https://stackoverflow.com/a/51840858
 
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "<>",
-    authDomain: "<>",
-    databaseURL: "<>",
-    projectId: "<>",
-    storageBucket: "<>",
-    messagingSenderId: "<>",
-    appId: "<>"
-  }
+    apiKey: urls.apiKey,
+    authDomain: urls.authDomain,
+    databaseURL: urls.databaseURL,
+    projectId: urls.projectId,
+    storageBucket: urls.storageBucket,
+    messagingSenderId: urls.messagingSenderId,
+    appId: urls.appId
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
